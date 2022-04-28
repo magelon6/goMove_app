b334430594af27866093815772bab88ab5b99903
Artem Kashuta <kashuta@gmail.com>
2022-04-28 16:15:49 +0300

    Initial server commit

---
+++ server/db/migrations/20220403131938-create-token.js
@@ -0,0 +1,35 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        onDelete: 'CASCADE',
+      },
+      refreshToken: {
+        type: Sequelize.TEXT,
+        required: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tokens');
+  },
+};
