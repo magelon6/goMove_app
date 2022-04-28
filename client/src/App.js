1953d3b3596985fe33c699a37b8cb87ccae3e596
Artem Kashuta <kashuta@gmail.com>
2022-04-28 14:50:19 +0300

    Initial client commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from "./components/NavBar/NavBar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+        <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h2>TSTS</h2>
+        </div>
+    );
+};
+
+export default NavBar;
