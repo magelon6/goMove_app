4b68827fd864440f61537b0b51b821f990e702ba
Artem Kashuta <kashuta@gmail.com>
2022-05-01 18:10:58 +0300

    Some refactored components

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,52 +1,45 @@
-//import { Input } from '@mui/material'
-import React from 'react'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCityFromDB } from '../../redux/actions/thunk/thunkCity'
+import React, {useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {getCityFromDB} from '../../redux/actions/thunk/thunkCity'
 
 import InputCenter from '../InputCenter/InputCenter'
+import StaticGraph from '../StaticGraph/StaticGraph'
+import Grid from "@mui/material/Grid";
 
 function Home() {
-
-  const price = useSelector((state) => state.price)
-
-
-  const dispatch = useDispatch()
-
-  
-useEffect(() => {
-  dispatch(getCityFromDB())
-},[])
-
-// useEffect(() => {
-//   dispatch(getPriceFromDB())
-// }, [])
+    const price = useSelector((state) => state.price)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getCityFromDB())
+    }, [])
 
 
+    return (
+        <div style={{margin: 30}}>
+            <Grid container>
+                <Grid item xs={12} sm={6} md={3}>
+                    <StaticGraph/>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <StaticGraph/>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <StaticGraph/>
+                </Grid>
+                <Grid item xs={12} sm={6} md={3}>
+                    <StaticGraph/>
+                </Grid>
+            </Grid>
+            <InputCenter/>
+            {price && price.map((el) =>
+                <div key={el.id} style={{border: '2px solid black'}}>
+                    <p> {el.name}</p>
+                    <p> {el.price}</p>
+                </div>
+            )}
+        </div>
 
-  return (
-   <>
-<center >
-
-
-
-<InputCenter/>
-
-   
-   {price && price.map((el)=>
-   <div key={el.id} style={{border: '2px solid black'}}>
-
-   <p> {el.name}</p>
-   <p> {el.price}</p>
-
-  
-   </div>
-)}
-</center>
-
-
-   </>
-  )
+    )
 }
 
 export default Home
--- client/src/components/InputCenter/InputCenter.jsx
+++ client/src/components/InputCenter/InputCenter.jsx
@@ -1,8 +1,7 @@
-import { Autocomplete, Button, TextField } from '@mui/material'
-import React from 'react'
-import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getPriceFromDB } from '../../redux/actions/thunk/thunkPrice'
+import {Autocomplete, Button, TextField} from '@mui/material'
+import React, {useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {getPriceFromDB} from '../../redux/actions/thunk/thunkPrice'
 
 function InputCenter(e) {
 
@@ -10,34 +9,29 @@ function InputCenter(e) {
   const [data, setData] = useState("")
   const dispatch = useDispatch()
 
+  const slice = city.map(el => ({id: el.id, label: `${el.city}, ${el.country}`}))
 
-  const slice = city.map(el => ({id: el.id, label: `${el.city}, ${ el.country}` }))
-
-  
   const searchCity = () => {
     let new1 = data.split(',')
     let new2 = {city: new1[0], country: new1[1]}
-    dispatch(getPriceFromDB(new2) )
+    dispatch(getPriceFromDB(new2))
   }
-  
 
-  
- 
   return (
-    <>
-    <Autocomplete
-  disablePortal
-  id="combo-box-demo"
-  options={slice}
-  sx={{ width: 300 }}
-  renderInput={(params) => <TextField {...params} label="Movie" />}
-  onChange={(e) => setData(e.target.innerText)}
-  
-/>
-<Button onClick={searchCity} variant="contained" color="success">
-  Success
-</Button>
-    </>
+      <>
+        <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={slice}
+            sx={{width: 300}}
+            renderInput={(params) => <TextField {...params} label="City"/>}
+            onChange={(e) => setData(e.target.innerText)}
+
+        />
+        <Button onClick={searchCity} variant="contained" color="success">
+          Find city
+        </Button>
+      </>
   )
 }
 
