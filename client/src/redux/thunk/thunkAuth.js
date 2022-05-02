4e9376d1389074e717b0b5eddd83aedb26682b13
Artem Kashuta <kashuta@gmail.com>
2022-05-02 17:27:15 +0300

    checkAuth front added

--- client/src/redux/thunk/thunkAuth.js
+++ client/src/redux/thunk/thunkAuth.js
@@ -1,5 +1,7 @@
 import AuthService from "../../services/AuthService";
-import {getUser} from "../actions/userActions";
+import {getUser, isAuth} from "../actions/userActions";
+import {API_URL} from "../../http";
+import axios from "axios";
 
 export const THUNK_ACTION_LOGIN = (userData) => async (dispatch) => {
     try {
@@ -10,3 +12,16 @@ export const THUNK_ACTION_LOGIN = (userData) => async (dispatch) => {
     }
 };
 
+
+export const THUNK_checkAuth = () => async (dispatch) => {
+    try {
+        const response = await axios.get(`${API_URL}/refresh`, {
+            withCredentials: true,
+        })
+        localStorage.setItem('token', response.data.accessToken)
+        dispatch(getUser({user: response.data.userFront}))
+        dispatch(isAuth());
+    } catch (e) {
+        console.log(e)
+    }
+};
