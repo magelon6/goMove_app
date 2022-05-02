2bb2f3cfcc36a4a3eef2997e8b711b05e5b942e2
Mikhail21 <saveshit8@gmail.com>
2022-05-02 18:23:27 +0300

    done hom2e.jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,40 @@
 import {Route, Routes} from 'react-router-dom';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Home from './components/Home/Home';
 import NavBar from "./components/NavBar/NavBar";
 import Registration from './components/Registration/Registration';
 import Login from "./components/Login/Login";
-import Authconfirm from './components/Registration/Authconfirm';
+import {THUNK_checkAuth} from "./redux/thunk/thunkAuth";
 
 
 function App() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [user, setUser] = useState();
+
+    const verify = () => {
+        if (THUNK_checkAuth()) {
+            setLoggedIn(true);
+            setUser(THUNK_checkAuth.user);
+            console.log('logged in');
+            return true;
+        } else {
+            console.log('not logged in');
+            return false;
+        }
+    }
+
+    useEffect(() => {
+        verify()
+    }, [loggedIn]);
+
+
     return (
         <div className="App">
             <NavBar/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/auth' element={<Login/>}/>
-
                 <Route path='/registration' element={<Registration/>}/>
-                <Route path='/authconfirm' element={<Authconfirm />} />
             </Routes>
         </div>
     );
