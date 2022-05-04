79fc4320d02c2fa13019f6c4939428861b054744
Artem Kashuta <kashuta@gmail.com>
2022-05-04 13:41:22 +0300

    Validation form added to Registration.js

--- client/src/components/Registration/Registration.js
+++ client/src/components/Registration/Registration.js
@@ -12,12 +12,12 @@ export default function Registration() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState({name: '', email: '', password: ''});
     const user = useSelector(state => state);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(THUNK_ACTION_REGISTER({name, email, password}));
-        console.log(user)
     }
 
 
@@ -40,39 +40,71 @@ export default function Registration() {
                         <Grid container spacing={2}>
                             <Grid item xs={12}>
                                 <TextField
-                                    required
                                     fullWidth
                                     id="name"
                                     label="User Name"
                                     name="name"
                                     autoComplete="name"
-                                    onChange={(e) => setName(e.target.value)}
-                                    value={name}
+                                    onChange={(e) => {
+                                            setError({name:''});
+                                            setName(e.target.value);
+                                        let reg = new RegExp(/^[A-Za-z\d_.]+$/).test(e.target.value);
+                                        if (!reg) {
+                                            setError({name: 'Name is not valid - spaces are not allowed'});
+                                        }
+                                    }}
+                                    value = {name}
+                                    required
+                                    error = {Boolean(error?.name)}
+                                    helperText={error?.name}
+                                    variant={'outlined'}
                                 />
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
-                                    required
                                     fullWidth
                                     id="email"
                                     label="Email Address"
                                     name="email"
-                                    autoComplete="email"
-                                    onChange={(e) => setEmail(e.target.value)}
-                                    value={email}
+                                    autoComplete="off"
+                                    onChange={(e) => {
+                                        setError({email: ''});
+                                        setEmail(e.target.value);
+                                        let reg = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/
+                                        ).test(e.target.value);
+                                        if (!reg) {
+                                            setError({email: 'Email is not valid, please enter a valid email'});
+                                            console.log(error.email);
+                                        }
+                                    }}
+                                    value = {email}
+                                    required
+                                    error = {Boolean(error?.email)}
+                                    helperText={error?.email}
+                                    variant={'outlined'}
                                 />
                             </Grid>
                             <Grid item xs={12}>
                                 <TextField
-                                    required
                                     fullWidth
                                     name="password"
                                     label="Password"
                                     type="password"
                                     id="password"
-                                    autoComplete="new-password"
-                                    onChange={(e) => setPassword(e.target.value)}
+                                    autoComplete="off"
+                                    onChange={(e) => {
+                                        setError({password: ''});
+                                        setPassword(e.target.value);
+                                        let pass = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/).test(e.target.value);
+                                        if (!pass) {
+                                            setError({password: 'Password should contain at least one digit, one uppercase and one lowercase letter, and must be at least 6 characters long'});
+                                        }
+                                    }}
                                     value={password}
+                                    required
+                                    error = {Boolean(error?.password)}
+                                    helperText={error?.password}
+                                    variant={'outlined'}
                                 />
                             </Grid>
                         </Grid>
