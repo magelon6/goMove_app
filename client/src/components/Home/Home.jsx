3f2777ecda061c51a3b4278c66b4f8574063e2ea
Magomed Adizov <98264766+magelon6@users.noreply.github.com>
2022-05-04 18:31:48 +0300

    Верстка как на авиасейлс (#42)

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {getCityFromDB} from '../../redux/thunk/thunkCity'
 
@@ -6,17 +6,46 @@ import InputCenter from '../InputCenter/InputCenter'
 import StaticGraph from '../StaticGraph/StaticGraph'
 import Grid from "@mui/material/Grid";
 
+import Chart from '../Chart/Chart'
+import Chart1 from '../SecondChart/SecondChart'
+
+import backgroundImg from '../../images/background.jpg'
+
+const divStyle = {
+    height: '100vh',
+    maxHeight: '840px',
+    position: 'relative',
+    transition: 'height 99999s',
+    backgroundImage: `url(${backgroundImg})`,
+    width: '100%',
+    backgroundRepeat: 'no-repeat',
+}
+
 function Home() {
     const price = useSelector((state) => state.price)
     const price2 = useSelector((state) => state.price2)
+    const [chart, setChart] = useState(price)
 
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getCityFromDB())
     }, [])
 
+    useEffect(() => {
+        console.log('111111111' , price);
+        setChart(!!price.length)
+      }, [price])
 
     return (
+        <>
+        <div style={divStyle}>
+            {/* <h1>Some text</h1> */}
+            <div className='input-wrapper' style={{ display: 'flex', flexDirection: 'column', height: '100%', textAlign: 'center', width: '100%'}}>
+                <div style={{display: 'flex',  justifyContent: 'center', marginTop: '25vh'}}>
+                    <InputCenter/>
+                </div>
+            </div>
+        </div>
         <div style={{margin: 30}}>
             <Grid container>
                 <Grid item xs={12} sm={6} md={3}>
@@ -32,7 +61,13 @@ function Home() {
                     <StaticGraph/>
                 </Grid>
             </Grid>
-            <InputCenter/>
+            {chart 
+            && 
+            <>
+            <Chart/>
+            <Chart1/>
+            </> 
+            }
             {/* <div className='d-flex '>
               <div width="300px">
                 {price && price.map((el) =>
@@ -62,6 +97,7 @@ function Home() {
               </div>
             </div> */}
         </div>
+        </>
 
     )
 }
