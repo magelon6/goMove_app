bfe0b5018f1d89dfa6e1bc9a156917a06c717c30
Ertine <ertine.rb@gmail.com>
2022-05-05 13:51:03 +0300

    исправил кнопку редиректа логина и авториз

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Navigate} from 'react-router-dom';
 import React, {useEffect, useState} from 'react';
 import Home from './components/Home/Home';
 import NavBar from "./components/NavBar/NavBar";
@@ -6,12 +6,15 @@ import Registration from './components/Registration/Registration';
 import Login from "./components/Login/Login";
 import {THUNK_checkAuth} from "./redux/thunk/thunkAuth";
 import UserProfile from './components/UserProfile/UserProfile';
+import { useSelector } from 'react-redux';
+
 
 import Chart from './components/Chart/Chart';
 import Chart2 from './components/Chart/Chart';
 
 
 function App() {
+    const isUser = useSelector(state=> state.user)
     const [loggedIn, setLoggedIn] = useState(false);
     const [user, setUser] = useState();
 
@@ -31,19 +34,32 @@ function App() {
         verify()
     }, [loggedIn]);
 
-
+  if (isUser) {   
     return (
         <div className="App">
             <NavBar/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
-                <Route path='/auth' element={<Login/>}/>
                 <Route path='/profile' element={<UserProfile/>}/>
-                <Route path='/registration' element={<Registration/>}/>
+                <Route path='*' element={<Navigate to='/' replace/>}/>
             </Routes>
 
         </div>
     );
+  } 
+  return (
+    <div className="App">
+        <NavBar/>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/auth' element={<Login/>}/>
+            <Route path='/profile' element={<UserProfile/>}/>
+            <Route path='/registration' element={<Registration />} />
+            <Route path='*' element={<Navigate to='/' replace/>}/>
+        </Routes>
+
+    </div>
+);
 }
 
 export default App;
