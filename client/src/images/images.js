f2d96df09106bc3700ca04db1ee7a48ca7d2f8de
Artem Kashuta <kashuta@gmail.com>
2022-05-09 11:22:30 +0300

    add: Header component

---
+++ client/src/images/images.js
@@ -0,0 +1,9 @@
+import background from './background.jpg';
+import logo from './logo.png';
+import logo_transparent from './logo_transparent.png';
+
+export default {
+    background,
+    logo,
+    logo_transparent
+};
