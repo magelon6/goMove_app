a7d7831e438e2c764e932c65bae6ee0bd205c791
Artem Kashuta <kashuta@gmail.com>
2022-05-10 06:03:49 +0300

    feat: blog api server side added

--- server/router/index.js
+++ server/router/index.js
@@ -4,7 +4,9 @@ const userController = require('../controller/user-controller');
 const authMiddleware = require('../middleware/auth.middleware');
 const apiData = require('../controller/api');
 const upload = require('../middleware/multer.middleware');
+const blogController = require('../controller/blog-controller');
 
+//auth routes
 router.post(
     '/registration',
     body('email').isEmail().withMessage('Email is not valid'),
@@ -18,12 +20,22 @@ router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
 router.get('/refresh', userController.refresh);
 router.get('/users', authMiddleware, userController.getUsers);
+
+// api
 router.get('/city', apiData.home);
 router.post('/price', apiData.price);
 router.get('/currency', apiData.currency);
+
+//user profile routes
 router.get('/userprofile/:id', userController.getUserProfile);
 router.patch('/userprofile/:id', upload.single('file'), userController.updateUserProfile);
 
-// router.post('/userprofile/:id/avatar', userController.updateUserAvatar);
+
+//blog routes
+router.get('/blog', blogController.getAllPosts);
+router.get('/blog/:id', blogController.getPostById);
+router.post('/blog', authMiddleware, blogController.createPost);
+router.patch('/blog/:id', authMiddleware, blogController.updatePost);
+router.delete('/blog/:id', authMiddleware, blogController.deletePost);
 
 module.exports = router;
